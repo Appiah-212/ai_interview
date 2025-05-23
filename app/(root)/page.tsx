1fa3ded7dd96e93b2487032f0707bcd7ce6ddc96
Appiah-212 <yawtawia212@gmail.com>
2025-05-23 03:46:14 +0100

    Showing generated forms

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,20 @@
 import InterviewCard from '@/components/InterviewCard'
 import { Button } from '@/components/ui/button'
-import { dummyInterviews } from '@/constants'
+import { getCurrentUser, getInterviewsByUserId, getLatestInterviews } from '@/lib/actions/auth.action'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const page = () => {
+const page = async () => {
+  const user = await getCurrentUser();
+  const [userInterviews, latestInterviews] = await Promise.all([
+    await getInterviewsByUserId(user?.id!),
+    await getLatestInterviews({ userId: user?.id!}) 
+  ])
+
+
+  const hasPastInterviews = userInterviews?.length > 0;
+  const hasUpcomingInterviews = latestInterviews?.length > 0
   return (
     <>
       <section className="card-cta">
@@ -28,11 +37,15 @@ const page = () => {
         <h2> Your Interviews</h2>
 
         <div className="interviews-section">
-          {dummyInterviews.map((interview) =>(
-            <InterviewCard {...interview} key={interview.id} />
-          ))}
+          {hasPastInterviews ?(
+                userInterviews?.map((interview) => (
+                  <InterviewCard {...interview} key={interview.id} />
+                ))) : (
+                     <p>You haven&apos;t taken any interviews</p> 
+                )}  
+          
 
-          {/* <p>You haven&apos;t taken any interviews</p> */}
+          
           
 
         </div>
@@ -42,9 +55,12 @@ const page = () => {
         <h2>Take an Interview</h2>
 
         <div className="interviews-section">
-        {dummyInterviews.map((interview) =>(
-            <InterviewCard {...interview} key={interview.id} />
-          ))}
+        {hasUpcomingInterviews ?(
+                latestInterviews?.map((interview) => (
+                  <InterviewCard {...interview} key={interview.id} />
+                ))) : (
+                     <p>There are no new interviews available</p> 
+                )}  
           
         </div>
       </section>
